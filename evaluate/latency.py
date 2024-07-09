8a41d0aef5d7ad33dce7cba0803b80aa087d4511
RicardoHalak <106038660+RicardoHalak@users.noreply.github.com>
2024-07-09 11:41:55 +0200

    Update latency.py

--- evaluate/latency.py
+++ evaluate/latency.py
@@ -46,65 +46,6 @@ def latency(model, tokenizer, n_run, n_tokens):
     print(f'Tokens/s: {tokens_per_sec:.1f}')
 
     return time_to_first_token, times, outputs
-
-
-# def latency(model, tokenizer, seqlen):
-
-#     print('Loading wikitext2...')
-#     data = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
-#     data = tokenizer("\n\n".join(data["text"]), return_tensors="pt")
-#     data = data.input_ids.to(model.device)
-
-#     # seqlen = 2048 # tokens_per_sequence
-#     model = model.eval()
-#     n_samples = data.numel() // seqlen
-
-#     latency = []
-#     with tqdm(range(n_samples), desc="Latency -") as progress_bar:
-#         for i in progress_bar:
-#             start_index = i * seqlen
-#             end_index = (i + 1) * seqlen
-#             input_seq = data[:, start_index:end_index].to(model.device)
-#             with torch.no_grad():
-#                 start_counter = time.time()
-#                 output = model(input_seq)
-#                 end_counter = time.time()
-#             curr_latency = (end_counter - start_counter) / seqlen
-#             latency.append(curr_latency)
-
-#             progress_bar.set_description(f"Latency {curr_latency * 1000:.3f} ms/token")
-#             print(f'Average tokens per second: {curr_latency**(-1)} tokens/s')
-
-#     avg_latency_per_token = np.mean(latency)
-#     print(f'Average latency per token: {avg_latency_per_token * 1000} ms/token')
-#     print(f'Average tokens per second: {avg_latency_per_token**(-1)} tokens/s')
-    
-#     return avg_latency_per_token
-
-# def latency2(model, tokenizer, seqlen):
-#     print('Loading wikitext2...')
-#     data = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
-#     data = tokenizer("\n\n".join(data["text"]), return_tensors="pt")
-#     input_ids = data.input_ids.to(model.device)
-
-#     # seqlen = 2048 # tokens_per_sequence
-#     model = model.eval()
-#     cache = {'past': None}
-
-#     with torch.no_grad():
-#         attention_mask = torch.ones((1, input_ids.numel()), device=model.device)
-#         times = []
-#         for i in range(input_ids.numel()):
-#             tick = time.time()
-#             # print(input_ids[:, i].reshape((1,-1)))
-#             out = model(
-#                 input_ids[:, i].reshape((1,-1)),
-#                 past_key_values=cache['past'],
-#                 # attention_mask=attention_mask[:, :(i + 1)].reshape((1, -1))
-#             )
-#             times.append(time.time() - tick)
-#             print(i, times[-1])
-           
 #             del out
 #         # import numpy as np
 #         print('Median:', np.median(times))
